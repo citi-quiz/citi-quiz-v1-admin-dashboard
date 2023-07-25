dd8eff6bebffb3bea549063aed8893621dc659db
Sri Vigneshwar DJ <srivicky2000@gmail.com>
2023-07-26 02:01:47 +0530

    control

--- src/backend.js
+++ src/backend.js
@@ -3,4 +3,4 @@ import axios from "axios";
 export const API_BACKEND_WEB_STAGING = "http://localhost:8092/api/web";
 export const API_BACKEND_WEB_PROD =
     "https://citi-quiz-apis.onrender.com/api/web";
-export const API_BACKEND_WEB = API_BACKEND_WEB_STAGING;
+export const API_BACKEND_WEB = API_BACKEND_WEB_PROD;
