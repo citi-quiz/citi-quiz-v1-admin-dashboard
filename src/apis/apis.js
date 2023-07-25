22f1e359ace4dd967d88142815063572ee168713
Sri Vigneshwar DJ <srivicky2000@gmail.com>
2023-07-25 21:03:15 +0530

    delete the user in controllers

--- src/apis/apis.js
+++ src/apis/apis.js
@@ -4,167 +4,176 @@ import { API_BACKEND_WEB } from "../backend";
 // ?? User Control ---------------------------
 
 export const getAllUsers = () => {
-  return axios
-    .get(`${API_BACKEND_WEB}/get/all/users`)
-    .then((res) => {
-      return res.data.users;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    return axios
+        .get(`${API_BACKEND_WEB}/get/all/users`)
+        .then((res) => {
+            return res.data.users;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
 
 export const getAUsers = (id) => {
-  return axios
-    .get(`${API_BACKEND_WEB}/get/a/user/${id}`)
-    .then((res) => {
-      return res.data.users;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    return axios
+        .get(`${API_BACKEND_WEB}/get/a/user/${id}`)
+        .then((res) => {
+            return res.data.users;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
+
+export const deleteAUser = (id) => {
+    console.log("id: -> ", id);
+    return axios.delete(`${API_BACKEND_WEB}/delete/user/${id}`).then((res) => {
+        return res;
+    }).catch((error) => {
+        console.log("Error - ", error);
+    });
+}
 
 // ?? -----------------------------------------
 
 // ?? Sets Section ------------------------------
 
 export const createCategorySet = (set) => {
-  return axios
-    .post(`${API_BACKEND_WEB}/setcategory/create`, set)
-    .then((res) => {
-      return res.data.cate;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    return axios
+        .post(`${API_BACKEND_WEB}/setcategory/create`, set)
+        .then((res) => {
+            return res.data.cate;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
 
 export const getAllCategorySets = () => {
-  return axios
-    .get(`${API_BACKEND_WEB}/setcategory/get/all`)
-    .then((res) => {
-      return res.data.cate;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    return axios
+        .get(`${API_BACKEND_WEB}/setcategory/get/all`)
+        .then((res) => {
+            return res.data.cate;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
 
 export const createSetOriginal = (set) => {
-  return axios
-    .post(`${API_BACKEND_WEB}/sets/create`, set)
-    .then((res) => {
-      return res.data.sets;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    return axios
+        .post(`${API_BACKEND_WEB}/sets/create`, set)
+        .then((res) => {
+            return res.data.sets;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
 
 export const editSetOriginal = (set, setId) => {
-  return axios
-    .post(`${API_BACKEND_WEB}/sets/update/${setId}`, set)
-    .then((res) => {
-      return res.data.sets;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    return axios
+        .post(`${API_BACKEND_WEB}/sets/update/${setId}`, set)
+        .then((res) => {
+            return res.data.sets;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
 
 export const disbaleSetOriginal = (set) => {
-  return axios
-    .post(`${API_BACKEND_WEB}/sets/disable`, set)
-    .then((res) => {
-      return res.data.sets;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    return axios
+        .post(`${API_BACKEND_WEB}/sets/disable`, set)
+        .then((res) => {
+            return res.data.sets;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
 
 export const enableSetOriginal = (set) => {
-  return axios
-    .post(`${API_BACKEND_WEB}/sets/enable`, set)
-    .then((res) => {
-      return res.data.sets;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    return axios
+        .post(`${API_BACKEND_WEB}/sets/enable`, set)
+        .then((res) => {
+            return res.data.sets;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
 
 // ?? Question Section ----------------------------
 
 export const createQuestion = (question) => {
-  return axios
-    .post(`${API_BACKEND_WEB}/question/create`, question)
-    .then((res) => {
-      return res.data.question;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    return axios
+        .post(`${API_BACKEND_WEB}/question/create`, question)
+        .then((res) => {
+            return res.data.question;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
 
 // ?? --------------------------------------------
 
 export const getAllSets = () => {
-  return axios
-    .get(`${API_BACKEND_WEB}/sets/get/all`)
-    .then((res) => {
-      return res.data.sets;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    return axios
+        .get(`${API_BACKEND_WEB}/sets/get/all`)
+        .then((res) => {
+            return res.data.sets;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
 
 export const getASet = () => {};
 
 export const getAllQuestions = () => {
-  return axios
-    .get(`${API_BACKEND_WEB}/question/get/all`)
-    .then((res) => {
-      return res.data.allquestion;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    return axios
+        .get(`${API_BACKEND_WEB}/question/get/all`)
+        .then((res) => {
+            return res.data.allquestion;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
 
 export const getAQuestion = () => {};
 
 export const getAllQuestionUnderSet = (setId) => {
-  return axios
-    .get(`${API_BACKEND_WEB}/question/get/all/set/${setId}`)
-    .then((res) => {
-      return res.data.allquestion;
-    })
-    .catch((err) => {
-      console.log("Error ", err);
-    });
+    return axios
+        .get(`${API_BACKEND_WEB}/question/get/all/set/${setId}`)
+        .then((res) => {
+            return res.data.allquestion;
+        })
+        .catch((err) => {
+            console.log("Error ", err);
+        });
 };
 
 export const deleteQuestionUnderSet = (qId) => {
-  return axios
-    .delete(`${API_BACKEND_WEB}/question/delete`, { data: qId })
-    .then((res) => {
-      return res.data.question;
-    })
-    .catch((err) => console.log(err));
+    return axios
+        .delete(`${API_BACKEND_WEB}/question/delete`, { data: qId })
+        .then((res) => {
+            return res.data.question;
+        })
+        .catch((err) => console.log(err));
 };
 
 export const editQuestionUnderSet = (qId) => {
-  return axios
-    .put(`${API_BACKEND_WEB}/question/update`, qId)
-    .then((res) => {
-      return res;
-    })
-    .catch((err) => {
-      console.log("Error -", err);
-    });
+    return axios
+        .put(`${API_BACKEND_WEB}/question/update`, qId)
+        .then((res) => {
+            return res;
+        })
+        .catch((err) => {
+            console.log("Error -", err);
+        });
 };
 
 export const getAUser = () => {};
@@ -172,14 +181,14 @@ export const getAUser = () => {};
 // ?? -----------------------------------------------
 
 export const getAllTests = () => {
-  return axios
-    .get(`${API_BACKEND_WEB}/test/get/all`)
-    .then((res) => {
-      return res.data.allTest;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    return axios
+        .get(`${API_BACKEND_WEB}/test/get/all`)
+        .then((res) => {
+            return res.data.allTest;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
 
 // ?? ------------------------------------------------
@@ -188,34 +197,34 @@ export const getAllTests = () => {
 
 // Notification
 export const sendNotificationsToUsers = (nofity) => {
-  return axios
-    .post(`${API_BACKEND_WEB}/notification/create`, nofity)
-    .then((res) => {
-      return res;
-    })
-    .catch((err) => {
-      console.log("Error -", err);
-    });
+    return axios
+        .post(`${API_BACKEND_WEB}/notification/create`, nofity)
+        .then((res) => {
+            return res;
+        })
+        .catch((err) => {
+            console.log("Error -", err);
+        });
 };
 
 export const getAllNotification = () => {
-  return axios.get(`${API_BACKEND_WEB}/get/all/notification`)
-  .then((res) => {
-    return res;
-  })
-  .catch((err) => {
-    console.log("Error -", err);
-  });
+    return axios.get(`${API_BACKEND_WEB}/get/all/notification`)
+        .then((res) => {
+            return res;
+        })
+        .catch((err) => {
+            console.log("Error -", err);
+        });
 }
 
 export const deleteNotificationHere = (notification) => {
-  return axios
-  .delete(`${API_BACKEND_WEB}/notification/delete`,{data: notification})
-  .then((res) => {
-    return res;
-  })
-  .catch((err) => {
-    console.log("Error -", err);
-  });
-}
-// ------------------------
+        return axios
+            .delete(`${API_BACKEND_WEB}/notification/delete`, { data: notification })
+            .then((res) => {
+                return res;
+            })
+            .catch((err) => {
+                console.log("Error -", err);
+            });
+    }
+    // ------------------------
--- src/pages/screens/UserDetails.js
+++ src/pages/screens/UserDetails.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { getAUsers, getAllUsers } from "../../apis/apis";
-import { Routes, Route, useParams } from "react-router-dom";
+import { deleteAUser, getAUsers, getAllUsers } from "../../apis/apis";
+import { Routes, Route, useParams, useNavigate } from "react-router-dom";
 import "./UserDetails.css";
 import { Avatar } from "@mui/material";
 import { deepOrange } from "@mui/material/colors";
+import Button from '@mui/material/Button';
 
 function UserDetails() {
   const { userId } = useParams();
@@ -20,9 +21,28 @@ function UserDetails() {
       });
   };
 
+  const [userStatus, setuserStatus] = useState(false);
+  const navigate = useNavigate();
+
+  const deleteTheUser = (e) => {
+      e.preventDefault();
+      if(userDetails){
+        console.log("User Details - ", userDetails._id);
+        deleteAUser(userDetails._id).then((res) => {
+          if(res.data){
+            navigate("/");
+            setuserStatus(true);
+          }
+      }).catch((error) => {
+          console.log("Error - ", error);
+      })
+      }
+  }
+
   useEffect(() => {
     getAllUsersDetails();
-  }, []);
+  }, [userStatus]);
+
 
   return (
     <div>
@@ -52,6 +72,14 @@ function UserDetails() {
       <p className="user-name" style={{textAlign: 'center'}}>{userDetails.email}</p>
       <p className="test-score" style={{textAlign: 'center'}}>No. Test Attended - <b>{userDetails.totalScore}</b></p>
       <p className="test-score" style={{textAlign: 'center'}}>User Token - <b>{userDetails.user_temp_id}</b></p>
+
+        <div style={{
+          marginTop:15
+        }}>
+          <Button variant="contained" onClick={deleteTheUser}>Delete This User</Button>
+        </div>
+
+
     </div>
   );
 }
